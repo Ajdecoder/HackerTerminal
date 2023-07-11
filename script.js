194b7743527a9b7685b33bed9b77aec275a3633d
Anuj Kumar Shrivastav <123423315+Ajdecoder@users.noreply.github.com>
2023-07-11 21:24:12 +0530

    webdeployed

---
+++ script.js
@@ -0,0 +1,179 @@
+
+let user = prompt("Enter User's Name To Proceed")
+
+if (user) {
+  let Name_code = [
+    `@4z6G&L`,
+    `@ry65df`,
+    `@099eri`,
+    `@343uer`,
+    `@9545tshs`,
+    `@756perf`,
+    `@5430yud`,
+    `@njsf65`,
+    `@543af64`,
+    `@usre235`,
+    `@533trys`,
+    `@358purs`,
+    `@6530ofs`,
+    `@420sfua`
+  ]
+  let pass = [
+    `9E*jn#h@7q`,
+
+    `$v4z6G&L!9`,
+
+    `3Y@uQ1^l9m`,
+
+    `#p7R@2bX6c`,
+
+    `$n5F&mZ*8k`,
+
+    `2C@v#6gT!1`,
+
+    `7Y&j#4tL*m`,
+
+    `$p6V@3wX!5`,
+
+    `4K@q#8jS!2`,
+
+    `$v5M6xG*9p`,
+
+    `6B@k#9zQ!3`,
+
+    `$c7W#2bL*p`,
+
+    `9H*j#6m@1q`,
+
+    `$s4T6nX*g8`,
+
+    `2D@v#7kG!1`,
+
+    `7L&p#4qR*m`,
+
+    `$w6F@3mZ!5`,
+
+    `4N@j#8gT!2`,
+
+    `$k5S6qX*9j`,
+
+    `6P@l#9hB!3`,
+
+    `$x7C#2vL*p`,
+
+    `9M*j#6b@1q`,
+
+    `$z4G6xW*g8`,
+
+    `2T@n#7rK!1`,
+
+    `7X&k#4vH*m`,
+
+    `$f6Q@3dM!5`,
+
+    `4Z@j#8tF!2`,
+
+    `$h5B6wR*p9`,
+
+    `6J@p#9cL!3`,
+
+    `$r7G#2vX*p`,
+
+    `9K*m#6h@1q`,
+
+    `$d4M6tF*g8`,
+
+    `2P@w#7sN!1`,
+  ]
+
+  let index = Math.floor(Math.random() * pass.length - 1)
+  let password = pass[index]
+
+  let i = Math.floor(Math.random() * Name_code.length - 1)
+  let Usr_Name = Name_code[i]
+
+  let arr = [
+    `$-> Access granted`,
+    `$-> Wait Connecting To The Server.....`,
+    `$-> Initializing the NITKO network.....`,
+    `$-> Fetching the network got failed retrying.....`,
+    `$-> Retrieving all the information.....`,
+    `$-> Trying to fetch all information from server.....`,
+    `$-> Trying to get ${user}'s Password`,
+    `$-> Password not found retrying please wait.....`,
+    `$-> Password accomplishment successfull`,
+    `$-> Initiating ${user}'s Facebook access.....`,
+    `$-> Unable to get access retrying....`,
+    `$-> Facebook username retrieving failed`,
+    `$-> Facebook username is ${user}${Usr_Name}`,
+    `$-> Facebook password retrieving....`,
+    `$-> password Accomplished successfully`,
+    `$-> Facebook password is ${password}`,
+    `$-> Instagram password retrieving....`,
+    `$-> Instagram password is ${password}`
+  ]
+
+
+  const sleep = async (sec) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(true)
+      }, 1000 * sec)
+    })
+  }
+
+  const HackingProg = async (cmd) => {
+    await sleep(1.5)
+    text.innerHTML = text.innerHTML + cmd + "<br>"
+  }
+
+  (async () => {
+    for (let i = 0; i < arr.length; i++) {
+      await HackingProg(arr[i])
+    }
+  })()
+}
+else {
+
+  let arr = [
+    `--------------->No Input ACCESS DENIED! 404-`,
+    `\t\t\t\t`,
+    `⚠️💀-->X-Originating-IP Failed.....`,
+    `⚠️💀-->X-Forwarded-For Failed.....`,
+    `⚠️💀-->X-Forwarded Failed.....`,
+    `⚠️💀-->Forwarded-For Failed.....`,
+    `⚠️💀-->X-Remote-IP Failed.....`,
+    `⚠️💀-->Client-IP Failed.....`,
+    `⚠️💀-->Cluster-Client-IP Failed.....`,
+    `⚠️💀-->X-ProxyUser-Ip Failed.....`,
+    `⚠️💀-->Host Failed.....`,
+    `⚠️💀-->error?:handle/–> HTTP 200 Denied`,
+    `⚠️💀-->prog.com//;//secret –> HTTP 200 Denied`,
+    `{"user_id":"<legit_id>","user_id":"<victims_id>"} (JSON Parameter Pollution)`,
+    `user_id=ATTACKER_ID&user_id=VICTIM_ID (Parameter Pollution)`
+  ]
+  const sleep = async (sec) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(true)
+      }, 1000 * sec)
+    })
+  }
+
+  const HackingProg = async (cmd) => {
+    await sleep(1.5)
+    text.innerHTML = text.innerHTML + cmd + "<br>"
+  }
+
+  (async () => {
+    for (let i = 0; i < arr.length; i++) {
+      await HackingProg(arr[i])
+    }
+  })()
+}
+
+
+
+
+
+
